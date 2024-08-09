5276ddfb789a2cef9c2410aa7b81996ce7ad2d60
probrahim <ibrahimhassani674@gmail.com>
2024-08-09 10:39:24 +0200

    all

--- src/Gestionemployes/Main.java
+++ src/Gestionemployes/Main.java
@@ -15,16 +15,18 @@ public class Main {
 
         Employes emp1 = new Employes(1, "Ali", "Ahmed", "123 Street", "ali@example.com", "123456789");
         Employes emp2 = new Employes(2, "Sara", "Mohammed", "456 Avenue", "sara@example.com", "987654321");
-
+        Employes tes1 = new Employes(3,"hiba","hiba","casa-fes","hiba@gmail.com","666");
         admin.addEmp(emp1);
         admin.addEmp(emp2);
-
+        admin.addEmp(tes1);
 
         Taches task1 = new Taches("Complete formulaire", 101, new Date(), new Date());
         Taches task2 = new Taches("Complete data", 102, new Date(), new Date());
+        Taches task3 = new Taches("Complete data", 103, new Date(), new Date());
 
         admin.assignTache(emp1, task1);
         admin.assignTache(emp2, task2);
+        admin.assignTache(tes1, task3);
 
         admin.getEmp();
 
